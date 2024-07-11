b48fec601fdabe1c9aeb0b9cb0aa215b021eb651
Marcello Teixeira <marcellogabriel2004@gmail.com>
2024-07-11 20:18:35 -0300

    Install frameworks

---
+++ BarberShop_Api/Domain/Models/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace BarberShop_Api.Domain.Models
+{
+    public interface IRepository<T> where T : class
+    {
+        T Get(int id);
+        void Post(T entity);
+        void Update(T up_entity);
+        void Delete(int id);
+    }
+}
