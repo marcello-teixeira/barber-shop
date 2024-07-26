c61cac7bff1a4257805bf5742b57666d4bbfb632
Marcello Teixeira <marcellogabriel2004@gmail.com>
2024-07-26 01:50:01 -0300

    Merge with AddAuthCustomer

--- BarberShop_Api/obj/Debug/net8.0/BarberShop_Api.AssemblyInfo.cs
+++ BarberShop_Api/obj/Debug/net8.0/BarberShop_Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BarberShop_Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3b463ba8af4da05b06cc78f054ef68870ca2919")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b66b7f5e36ca7dec31c1c4a8164fc74ba092a0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BarberShop_Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BarberShop_Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
