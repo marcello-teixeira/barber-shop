1ae0c3968c7acad4d3bb2147e5967990aaf9c66b
Marcello Teixeira <marcellogabriel2004@gmail.com>
2024-07-26 01:42:02 -0300

    Add auth company and customer login

---
+++ BarberShop_Api/Application/ViewModel/CompanyViewModel/CompanyViewPost.cs
@@ -0,0 +1,15 @@
+﻿namespace BarberShop_Api.Application.ViewModel.CompanyViewModel
+{
+    public class CompanyViewPost
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Location { get; set; }
+        public required string Login { get; set; }
+        public required string Password { get; set; }
+        public required string CNPJ { get; set; }
+        public string? Photo { get; set; }
+        public required string Email { get; set; }
+        public required string Phone { get; set; }
+    }
+}
