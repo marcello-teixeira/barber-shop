372be5f04355aa1bccf277d1b124ba358515d15d
Marcello Teixeira <marcellogabriel2004@gmail.com>
2024-08-04 17:26:12 -0300

    Profile Picture Finish

--- BarberShop_Api/obj/Debug/net8.0/BarberShop_Api.AssemblyInfo.cs
+++ BarberShop_Api/obj/Debug/net8.0/BarberShop_Api.AssemblyInfo.cs
@@ -18,6 +18,7 @@ using System.Reflection;
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ae0c3968c7acad4d3bb2147e5967990aaf9c66b")]
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f93edfd8e645bf77c4622a3527ed65956abaf186")]
@@ -31,6 +32,9 @@ using System.Reflection;
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d524cace3541ea063900b055427222b60ffdd1a")]
 >>>>>>> 6b93a3b (AddCors)
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b93a3bab35bb732e897c087ed03ee3f6ac45272")]
+>>>>>>> a445dc1 (Profile Picture Finish)
 [assembly: System.Reflection.AssemblyProductAttribute("BarberShop_Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BarberShop_Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
