d6f59446b031c4637d506f2e665133ab9c860719
Marcello Teixeira <marcellogabriel2004@gmail.com>
2024-08-06 12:35:40 -0300

    Debug altered

--- BarberShop_Api/obj/Debug/net8.0/BarberShop_Api.AssemblyInfo.cs
+++ BarberShop_Api/obj/Debug/net8.0/BarberShop_Api.AssemblyInfo.cs
@@ -22,6 +22,7 @@ using System.Reflection;
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ae0c3968c7acad4d3bb2147e5967990aaf9c66b")]
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f93edfd8e645bf77c4622a3527ed65956abaf186")]
@@ -47,6 +48,9 @@ using System.Reflection;
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+889ef22af526910e66c07f04c2796dd7b1032509")]
 >>>>>>> 2dbef40 (Controllers finished)
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef0e56a635f528c7a756d2fefe96c2e982249520")]
+>>>>>>> 6c9f902 (Debug altered)
 [assembly: System.Reflection.AssemblyProductAttribute("BarberShop_Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BarberShop_Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
