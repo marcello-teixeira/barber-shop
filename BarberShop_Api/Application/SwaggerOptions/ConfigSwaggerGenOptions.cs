348ad19bc98701bdfe8a190766fda3af0c52da4f
Marcello Teixeira <marcellogabriel2004@gmail.com>
2024-08-26 18:00:49 -0300

    Swagger Folder Add

---
+++ BarberShop_Api/Application/SwaggerOptions/ConfigSwaggerGenOptions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Asp.Versioning.ApiExplorer;
+
+namespace BarberShop_Api.Application.SwaggerOptions
+{
+    public class ConfigSwaggerGenOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider _ApiDescriptorProvide;
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach(var desc in _ApiDescriptorProvide)
+            {
+                options.SwaggerDoc(desc.GroupName);
+            }
+        }
+
+        public OpenApiInfo CreateOpenApiInfo(ApiVersionDescriptor desc)
+        {
+            OpenApiInfo openApiInfo = new();
+        }
+    }
+}
--- BarberShop_Api/obj/Debug/net8.0/BarberShop_Api.AssemblyInfo.cs
+++ BarberShop_Api/obj/Debug/net8.0/BarberShop_Api.AssemblyInfo.cs
@@ -35,6 +35,7 @@ using System.Reflection;
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ae0c3968c7acad4d3bb2147e5967990aaf9c66b")]
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f93edfd8e645bf77c4622a3527ed65956abaf186")]
@@ -99,6 +100,9 @@ using System.Reflection;
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bfcd6f37bff5dd3d317aa8c9f9d4f54d444465a")]
 >>>>>>> 9730d73 (Alter gitignore)
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9730d7328065e05a4431f3d77fc6a38347c7dc98")]
+>>>>>>> b1806ab (Swagger Folder Add)
 [assembly: System.Reflection.AssemblyProductAttribute("BarberShop_Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BarberShop_Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
